3bc7fc1950ecb50f99776b53677318ebe5a6e058
Matt <mattgrenier@outlook.com>
2026-09-05 21:16:32 -0500

    More layout work, fixed Navigate Up bug for files on source preview

--- Portfolio.Data/Migrations/20260825231735_UpdatePortfolioModel.Designer.cs
+++ Portfolio.Data/Migrations/20260825231735_UpdatePortfolioModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Portfolio.Data.Data;
 
@@ -11,9 +12,11 @@ using Portfolio.Data.Data;
 namespace Portfolio.Data.Migrations
 {
     [DbContext(typeof(PortfolioDbContext))]
-    partial class PortfolioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260825231735_UpdatePortfolioModel")]
+    partial class UpdatePortfolioModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
